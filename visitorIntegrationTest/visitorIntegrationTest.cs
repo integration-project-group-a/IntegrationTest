8a7d6bfa59adc28d5ab01cbea3ff8aceca17b131
antheboets <44194665+antheboets@users.noreply.github.com>
2019-04-25 14:01:09 +0200

    adding local and rabbitmq integration test for visitor

---
+++ visitorIntegrationTest/visitorIntegrationTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.XPath;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+class VisitorIntegrationTest
+{
+    private static String recivedMessage;
+
+
+
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "10.3.56.27", UserName = "manager", Password = "ehb" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+            var queueName = channel.QueueDeclare().QueueName;
+            channel.QueueBind(queue: queueName, exchange: "logs", routingKey: "");
+
+            Console.WriteLine("Waiting for Tests.");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                Console.WriteLine("Received message");
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(Encoding.UTF32.GetString(ea.Body));
+                recivedMessage = Encoding.UTF32.GetString(ea.Body);
+
+                doc.Schemas.Add(null, "../../../XSD/VisitorV2.xsd");
+
+                doc.Validate(ValidationCallBack);
+
+                
+            };
+            channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+            Console.ReadLine();
+        }
+      
+
+        Console.ReadKey();
+    }
+    private static void ValidationCallBack(object sender, ValidationEventArgs args)
+    {
+        if (args.Severity == XmlSeverityType.Warning)
+            Console.WriteLine("\tWarning: Matching schema not found.  No validation occurred." + args.Message);
+        else
+            Console.WriteLine("\tValidation error: " + args.Message);
+
+        HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create("http://10.3.56.26:9200/visitortest/invalidxml"); //url
+        httpWebRequest.ContentType = "application/json"; //ContentType
+        httpWebRequest.Method = "POST"; //Methode
+
+        //body
+        using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+        {
+            streamWriter.Write("{\"message\": \""+ recivedMessage.Replace("\"", "'") + "\"}");
+            Console.WriteLine("{\"message\": \"" + recivedMessage.Replace("\"","'") + "\"}");
+            streamWriter.Flush();
+            streamWriter.Close();
+        }
+
+        httpWebRequest.GetResponse(); //sending request
+    }
+}
---
+++ visitorIntegrationTestLocal/visitorIntegrationTestLocal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace visitorIntegrationTest
+{
+    class VisitorIntegrationTestLocal
+    {
+        public static void Main()
+        {
+            Console.WriteLine("local test");
+            XmlDocument doc1 = new XmlDocument();
+            doc1.Load("../../../XML/VisitorV2.xml");
+
+            doc1.Schemas.Add(null, "../../../XSD/VisitorV2.xsd");
+
+
+            doc1.Validate(ValidationCallBack);
+
+
+            XmlDocument doc2 = new XmlDocument();
+            doc2.Load("../../../XML/VisitorV2Invalid.xml");
+
+            doc2.Schemas.Add(null, "../../../XSD/VisitorV2.xsd");
+
+            doc2.Validate(ValidationCallBack);
+
+
+            Console.ReadKey();
+        }
+        private static void ValidationCallBack(object sender, ValidationEventArgs args)
+        {
+            if (args.Severity == XmlSeverityType.Warning)
+                Console.WriteLine("\tWarning: Matching schema not found.  No validation occurred." + args.Message);
+            else
+                Console.WriteLine("\tValidation error: " + args.Message);
+        }
+    }
+}
